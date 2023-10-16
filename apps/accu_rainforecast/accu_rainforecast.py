0230946adc65eb88cd034b23f02b129fd2ad0425
Rudi Badenhorst <rudib@property24.com>
2023-10-16 18:33:36 +0200

    wip

--- apps/accu_rainforecast/accu_rainforecast.py
+++ apps/accu_rainforecast/accu_rainforecast.py
@@ -4,16 +4,15 @@ import datetime
 
 class Get_Accu_RainForeCast(hass.Hass):
     def initialize(self):
+        
+        self.log("accu_rainforecast initializing...")
+        self.set_state( "sensor.acc_rain_prob", state=str(datetime.time.second), replace=True, attributes={"friendly_name": "AccuWeather Rain Probability"})
 
-        self.set_state( 'sensor.acc_rain_prob', state=datetime.time.second, replace=True, attributes={"friendly_name": 'AccuWeather Rain Probability'})
-
-        self.listen_state(self.set_acc_sensors)
-
+        self.get_state('sensor.acc_rain_prob');
         # Run every two minutes  
         self.run_every(self.callback, time, 2 * 60)
 
-    def callback(self, cb_args):       
-        self.set_state( 'sensor.acc_rain_prob', state=datetime.time.second, replace=True, attributes={"friendly_name": 'AccuWeather Rain Probability'})
-    
-    def set_acc_sensors(self, entity, attribute, old, new, kwargs):
-        self.set_state( 'sensor.acc_rain_prob', state=datetime.time.second, replace=True, attributes={"friendly_name": 'AccuWeather Rain Probability'})
+    def callback(self, cb_args):
+        newState = str(datetime.time.second)
+        self.log("accu_rainforecast: setting state " + newState)
+        self.set_state( "sensor.acc_rain_prob", state=newState, replace=True, attributes={"friendly_name": "AccuWeather Rain Probability"})
